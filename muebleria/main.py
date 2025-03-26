887a48c78d994d1376276417533c14f9ef4a13d0
Miguel <migue2801224@gmail.com>
2025-03-26 00:32:22 -0500

    Importancion subclases, rich y creacion de consola

--- muebleria/main.py
+++ muebleria/main.py
@@ -1,2 +1,10 @@
 # ejecutar el programa y probar las clases
+#importamos las subclases de mueble.py
+from mueble import Silla, Mesa, Armario
+from rich.console import Console
+from rich.table import Table
+
+#creacion consola formateada
+Console = Console()
+
 
